b71add039f8416f2c5cd21bf977b4ec3941a6390
George Higbie <gehigbie@gmail.com>
2017-01-14 23:40:07 -0700

    added varibles to be passed in

--- js.js
+++ js.js
@@ -13,7 +13,7 @@ function myForEach(array, func){
 	//loop through array and call func for each item in the array
 	for(var i = 0; i < array.length; i++){
 		//call func() for each item in array
-		func();
+		func(array[i]); //this calls this funuction, a generic function for each item of an array
 	}
 }
 
